3761de823340d96a30da61643215455977ded591
YusukeBou <76899801+YusukeBou@users.noreply.github.com>
2021-01-03 18:44:31 +0900

    Add files via upload

---
+++ NoteShield/NoteShield.ServiceWorker.js
@@ -0,0 +1,31 @@
+const cacheName = 'NoteShieldV1'; // Not pushing the updates but the user may actively fetch the update.
+
+const appFiles = [
+  "/NoteShield/",
+  "/NoteShield/NoteShield.html",
+  "/NoteShield/NoteShield.64x64.png",
+];
+
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(self.caches.open(cacheName).then(function(cache) {
+    return cache.addAll(appFiles);
+  }));
+});
+
+self.addEventListener('fetch', function(event) {
+  e.respondWith(
+    caches.match(event.request).then(function(request) {
+      if (request) {
+        return request;
+      }
+      // else: cache miss
+      return fetch(event.request).then(function(response) {
+        return caches.open(cacheName).then(function(cache) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
