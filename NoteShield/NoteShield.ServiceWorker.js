3f8470aaa5bd6ad459f7444e2a866a294e3d5d6c
YusukeBou <76899801+YusukeBou@users.noreply.github.com>
2021-01-03 21:48:10 +0900

    Add files via upload

--- NoteShield/NoteShield.ServiceWorker.js
+++ NoteShield/NoteShield.ServiceWorker.js
@@ -1,30 +1,80 @@
-const cacheName = 'NoteShieldV1'; // Not pushing the updates but the user may actively fetch the update.
+function resolvablePromise() {
+  this.resolve = undefined;
+  this.reject = undefined;
+  this.promise = new Promise(function(resolutionFunc, rejectionFunc){
+    this.resolve = resolutionFunc;
+    this.reject = rejectionFunc;
+  });
+}
+
+const jsonCacheName = "NoteShieldJson";
+const jsonFile = "/NoteShield/NoteShield.json";
+
+const jsonPromise = null;
+function getJsonAsync() {
+  if (!jsonPromise) {
+    jsonPromise = new resolvablePromise();
+    (async function(){
+      // First try online.
+      try {
+        const jsonResponse = await self.fetch(jsonFile);
+        const json = await jsonResponse.json();
+        const jsonCache = await self.caches.open(jsonCacheName);
+        await cache.put(jsonFile, jsonResponse.clone());
+        jsonPromise.resolve(json);
+      } catch (errorOnline) {
+        // Next try cache.
+        try {
+          await cachedResponse = self.caches.match(jsonFile);
+          const json = await cachedResponse.json();
+          jsonPromise.resolve(json);
+        } catch (errorCache) {
+          throw errorOnline; // Cache mishit is not an error.
+        }
+      }
+    })();
+  }
+  return jsonPromise;
+}
+
+async function getCacheNameAsync() {
+  const json = await getJsonAsync();
+  return `NoteShieldV${json.version}`;
+}
 
 const appFiles = [
   "/NoteShield/NoteShield.html",
   "/NoteShield/NoteShield.64x64.png",
+  "/NoteShield/NoteShield.120x120.png",
+  "/NoteShield/NoteShield.180x180.png",
 ];
 
+self.addEventListener("install", function(event) {
+  event.waitUntil((async function(){
+    const jsonCache = await self.caches.open(jsonCacheName);
+    await jsonCache.add(jsonCache);
+    const cacheName = await getCacheNameAsync();
+    const cache = await self.caches.open(cacheName);
+    await cache.addAll(appFiles);
+  })());
+}
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(self.caches.open(cacheName).then(function(cache) {
-    return cache.addAll(appFiles);
-  }));
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(request) {
-      if (request) {
-        return request;
+self.addEventListener("fetch", function(event) {
+  if (event.request.url === jsonFile) {
+    event.respondWith(getJsonAsync());
+  } else {
+    event.respondWith((async function(){
+      // First try cache.
+      const cachedResponse = await self.caches.match(event.request);
+      if (cachedResponse) {
+        return cachedResponse;
       }
-      // else: cache miss
-      return fetch(event.request).then(function(response) {
-        return caches.open(cacheName).then(function(cache) {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      });
-    })
-  );
+      // else: try online.
+      const response = await self.fetch(event.request);
+      const cacheName = await getCacheNameAsync();
+      const cache = await caches.open(cacheName);
+      await cache.put(event.request, response.clone());
+      return response;
+    })());
+  }
 });
